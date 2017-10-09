153254cf5e5c8358725297f68e10fb24a88cfe2a
Austin White <austingym@gmail.com>
2017-10-09 14:35:24 -0700

    🐠

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCy5uS0Sn_PaHPBroG4JVuN-nxFnuiWJRg",
+  authDomain: "catch-of-the-day-austinw.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-austinw.firebaseio.com",
+});
+
+export default base;
